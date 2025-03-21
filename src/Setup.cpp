cc3795b413b78869d8754b77f0ed71e63a49c271
LMauricius <mauriciosmit100@gmail.com>
2025-03-21 13:10:09 +0100

    Enabling OpenGL renderer

--- src/Setup.cpp
+++ src/Setup.cpp
@@ -1,8 +1,13 @@
 #include "VitraePluginOpenGL/Setup.hpp"
 
+#include "VitraePluginOpenGL/Specializations/Renderer.hpp"
+
 namespace VitraePluginOpenGL
 {
 
-void setup(Vitrae::ComponentRoot &root) {}
+void setup(Vitrae::ComponentRoot &root)
+{
+    root.setComponent<Vitrae::Renderer>(new Vitrae::OpenGLRenderer(root));
+}
 
 } // namespace VitraePluginOpenGL
