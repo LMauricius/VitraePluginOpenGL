95333f1178d3283861a7f6da9621a5d461814d19
LMauricius <mauriciosmit100@gmail.com>
2025-03-29 18:34:14 +0100

    Moved component registration to the setup function for more control

--- src/Setup.cpp
+++ src/Setup.cpp
@@ -1,13 +1,52 @@
 #include "VitraePluginOpenGL/Setup.hpp"
 
+#include "Vitrae/Assets/Material.hpp"
+
 #include "VitraePluginOpenGL/Specializations/Renderer.hpp"
+
+#include "VitraePluginOpenGL/Specializations/Compositing/ClearRender.hpp"
+#include "VitraePluginOpenGL/Specializations/Compositing/Compute.hpp"
+#include "VitraePluginOpenGL/Specializations/Compositing/DataRender.hpp"
+#include "VitraePluginOpenGL/Specializations/Compositing/SceneRender.hpp"
+#include "VitraePluginOpenGL/Specializations/FrameStore.hpp"
+#include "VitraePluginOpenGL/Specializations/Mesh.hpp"
+#include "VitraePluginOpenGL/Specializations/ShaderCompilation.hpp"
+#include "VitraePluginOpenGL/Specializations/Shading/Constant.hpp"
+#include "VitraePluginOpenGL/Specializations/Shading/FunctionCall.hpp"
+#include "VitraePluginOpenGL/Specializations/Shading/Header.hpp"
+#include "VitraePluginOpenGL/Specializations/Shading/Snippet.hpp"
+#include "VitraePluginOpenGL/Specializations/SharedBuffer.hpp"
+#include "VitraePluginOpenGL/Specializations/Texture.hpp"
+
+#include "dynasma/cachers/basic.hpp"
+#include "dynasma/keepers/naive.hpp"
+#include "dynasma/managers/basic.hpp"
 
 namespace VitraePluginOpenGL
 {
 
 void setup(Vitrae::ComponentRoot &root)
 {
-    root.setComponent<Vitrae::Renderer>(new Vitrae::OpenGLRenderer(root));
+    using namespace Vitrae;
+
+    root.setComponent<Renderer>(new OpenGLRenderer(root));
+
+    // clang-format off
+    root.setComponent<              MeshKeeper>(new  dynasma::NaiveKeeper<              MeshKeeperSeed, std::allocator<              OpenGLMesh>>());
+    root.setComponent<          MaterialKeeper>(new  dynasma::NaiveKeeper<          MaterialKeeperSeed, std::allocator<                Material>>());
+    root.setComponent<          TextureManager>(new dynasma::BasicManager<                 TextureSeed, std::allocator<           OpenGLTexture>>());
+    root.setComponent<       FrameStoreManager>(new dynasma::BasicManager<              FrameStoreSeed, std::allocator<        OpenGLFrameStore>>());
+    root.setComponent<   RawSharedBufferKeeper>(new  dynasma::NaiveKeeper<   RawSharedBufferKeeperSeed, std::allocator<   OpenGLRawSharedBuffer>>());
+    root.setComponent<    ShaderConstantKeeper>(new  dynasma::NaiveKeeper<    ShaderConstantKeeperSeed, std::allocator<    OpenGLShaderConstant>>());
+    root.setComponent<     ShaderSnippetKeeper>(new  dynasma::NaiveKeeper<     ShaderSnippetKeeperSeed, std::allocator<     OpenGLShaderSnippet>>());
+    root.setComponent<ShaderFunctionCallKeeper>(new  dynasma::NaiveKeeper<ShaderFunctionCallKeeperSeed, std::allocator<OpenGLShaderFunctionCall>>());
+    root.setComponent<      ShaderHeaderKeeper>(new  dynasma::NaiveKeeper<      ShaderHeaderKeeperSeed, std::allocator<      OpenGLShaderHeader>>());
+    root.setComponent<ComposeSceneRenderKeeper>(new  dynasma::NaiveKeeper<ComposeSceneRenderKeeperSeed, std::allocator<OpenGLComposeSceneRender>>());
+    root.setComponent< ComposeDataRenderKeeper>(new  dynasma::NaiveKeeper< ComposeDataRenderKeeperSeed, std::allocator< OpenGLComposeDataRender>>());
+    root.setComponent<    ComposeComputeKeeper>(new  dynasma::NaiveKeeper<    ComposeComputeKeeperSeed, std::allocator<    OpenGLComposeCompute>>());
+    root.setComponent<ComposeClearRenderKeeper>(new  dynasma::NaiveKeeper<ComposeClearRenderKeeperSeed, std::allocator<OpenGLComposeClearRender>>());
+    root.setComponent<CompiledGLSLShaderCacher>(new  dynasma::BasicCacher<CompiledGLSLShaderCacherSeed, std::allocator<      CompiledGLSLShader>>());
+    // clang-format on
 }
 
 } // namespace VitraePluginOpenGL
--- src/Specializations/Renderer.cpp
+++ src/Specializations/Renderer.cpp
@@ -1,27 +1,12 @@
 #include "VitraePluginOpenGL/Specializations/Renderer.hpp"
 
-#include "VitraePluginOpenGL/Specializations/Compositing/ClearRender.hpp"
-#include "VitraePluginOpenGL/Specializations/Compositing/Compute.hpp"
-#include "VitraePluginOpenGL/Specializations/Compositing/DataRender.hpp"
-#include "VitraePluginOpenGL/Specializations/Compositing/SceneRender.hpp"
-#include "VitraePluginOpenGL/Specializations/FrameStore.hpp"
-#include "VitraePluginOpenGL/Specializations/Mesh.hpp"
-#include "VitraePluginOpenGL/Specializations/ShaderCompilation.hpp"
-#include "VitraePluginOpenGL/Specializations/Shading/Constant.hpp"
-#include "VitraePluginOpenGL/Specializations/Shading/FunctionCall.hpp"
-#include "VitraePluginOpenGL/Specializations/Shading/Header.hpp"
 #include "VitraePluginOpenGL/Specializations/Shading/Snippet.hpp"
 #include "VitraePluginOpenGL/Specializations/SharedBuffer.hpp"
 #include "VitraePluginOpenGL/Specializations/Texture.hpp"
 
-#include "Vitrae/Assets/Material.hpp"
 #include "Vitrae/Collections/ComponentRoot.hpp"
 #include "Vitrae/Collections/MethodCollection.hpp"
 #include "Vitrae/Params/Standard.hpp"
-
-#include "dynasma/cachers/basic.hpp"
-#include "dynasma/keepers/naive.hpp"
-#include "dynasma/managers/basic.hpp"
 
 #include "glad/glad.h"
 // must be after glad.h
@@ -144,23 +129,6 @@ void OpenGLRenderer::mainThreadSetup(ComponentRoot &root)
     // threading stuff
     m_mainThreadId = std::this_thread::get_id();
     m_contextMutex.lock();
-
-    // clang-format off
-    root.setComponent<              MeshKeeper>(new  dynasma::NaiveKeeper<              MeshKeeperSeed, std::allocator<              OpenGLMesh>>());
-    root.setComponent<          MaterialKeeper>(new  dynasma::NaiveKeeper<          MaterialKeeperSeed, std::allocator<                Material>>());
-    root.setComponent<          TextureManager>(new dynasma::BasicManager<                 TextureSeed, std::allocator<           OpenGLTexture>>());
-    root.setComponent<       FrameStoreManager>(new dynasma::BasicManager<              FrameStoreSeed, std::allocator<        OpenGLFrameStore>>());
-    root.setComponent<   RawSharedBufferKeeper>(new  dynasma::NaiveKeeper<   RawSharedBufferKeeperSeed, std::allocator<   OpenGLRawSharedBuffer>>());
-    root.setComponent<    ShaderConstantKeeper>(new  dynasma::NaiveKeeper<    ShaderConstantKeeperSeed, std::allocator<    OpenGLShaderConstant>>());
-    root.setComponent<     ShaderSnippetKeeper>(new  dynasma::NaiveKeeper<     ShaderSnippetKeeperSeed, std::allocator<     OpenGLShaderSnippet>>());
-    root.setComponent<ShaderFunctionCallKeeper>(new  dynasma::NaiveKeeper<ShaderFunctionCallKeeperSeed, std::allocator<OpenGLShaderFunctionCall>>());
-    root.setComponent<      ShaderHeaderKeeper>(new  dynasma::NaiveKeeper<      ShaderHeaderKeeperSeed, std::allocator<      OpenGLShaderHeader>>());
-    root.setComponent<ComposeSceneRenderKeeper>(new  dynasma::NaiveKeeper<ComposeSceneRenderKeeperSeed, std::allocator<OpenGLComposeSceneRender>>());
-    root.setComponent< ComposeDataRenderKeeper>(new  dynasma::NaiveKeeper< ComposeDataRenderKeeperSeed, std::allocator< OpenGLComposeDataRender>>());
-    root.setComponent<    ComposeComputeKeeper>(new  dynasma::NaiveKeeper<    ComposeComputeKeeperSeed, std::allocator<    OpenGLComposeCompute>>());
-    root.setComponent<ComposeClearRenderKeeper>(new  dynasma::NaiveKeeper<ComposeClearRenderKeeperSeed, std::allocator<OpenGLComposeClearRender>>());
-    root.setComponent<CompiledGLSLShaderCacher>(new  dynasma::BasicCacher<CompiledGLSLShaderCacherSeed, std::allocator<      CompiledGLSLShader>>());
-    // clang-format on
 
     glfwInit();
 
